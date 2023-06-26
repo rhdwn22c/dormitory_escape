f37d1ceb045bded80a33e4902b665f83200861dd
rhdwn22c <rhdwn22c@gmail.com>
2023-06-26 23:20:47 +0900

    effect

--- Assets/Scripts/PlayerChat.cs
+++ Assets/Scripts/PlayerChat.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class PlayerChat : MonoBehaviour
+
+public class Playerchat : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Player player;
+    public Text[] playerText;
 
-    // Update is called once per frame
+    int i = 0;
+
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown) {
+            Destroy(playerText[i]);
+            Instantiate(playerText[i + 1], new Vector3(0, 0, 0), Quaternion.identify);
+            i++;
+        }
+
     }
 }
